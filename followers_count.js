47959af7b11cbd433ed59e3ae3c3abdee7344e94
adrian131 <adrian.tulhina@yshoo.com>
2025-11-24 15:31:51 -0500

    lab

---
+++ followers_count.js
@@ -0,0 +1,14 @@
+let count = 0;
+
+function increaseCount(){
+    count++;
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML = count;
+}
+
+function increaseCount() {
+    count++;
+    displayCount();
+}
